2cc9619e9476e5e2696788b786ab85d84617c145
thirishwesin <211phoothiri@gmail.com>
2020-09-13 10:42:35 +0630

    update api

--- routes/api.php
+++ routes/api.php
@@ -18,14 +18,21 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiresource('brands','Api\BrandController');
-Route::apiresource('categories','Api\CategoryController');
-Route::apiresource('subcategories','Api\SubcategoryController');
-Route::apiresource('items','Api\ItemController');
-Route::apiresource('users','Api\UserController');
+Route::prefix('v1')->group(function () {
+Route::apiResource('brands','Api\BrandController');
+Route::apiResource('categories','Api\CategoryController');
+Route::apiResource('subcategories','Api\SubcategoryController');
+Route::apiResource('items','Api\ItemController');
+Route::post('register','Api\AuthController@register')->name('register');
+Route::apiResource('users','Api\UserController');
 Route::get('filter_item/{sid}/{bid}','Api\ItemController@filter')->name('filter_item');
-Route::get('filter_item','Api\ItemController@filterQuery')->name('filter_item');
+Route::get('search_item','Api\ItemController@search');
+Route::get('itemByBrand','Api\ItemController@byBrand')->name('itemByBrand');
 
+  Route::get('itemBySubcategory','Api\ItemController@bySubcategory')->name('itemBySubcategory');
 
+Route::apiresource('orders','Api\OrderController');
+
+});
 
 
